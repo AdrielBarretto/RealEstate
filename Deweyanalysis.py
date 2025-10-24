c5458c159d06f8550583968e5c49c642d7bc5522
Adriel Barretto <addybarretto@gmail.com>
2025-10-24 17:45:25 -0400

    maybe

--- Deweyanalysis.py
+++ Deweyanalysis.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import tensorflow as tf
 import keras
+import gc
 import matplotlib.pyplot as plt
 
 sample = pd.read_csv("a.csv")
@@ -68,10 +69,67 @@ average2 = np.mean(neural_mse_list)
 print(average)
 print(average2)
 '''
-a = pd.get_dummies(cleaned['ZIP'],drop_first = False)
-pca = sc.decomposition.PCA(200)
-#pca.fit(a)
-#print(pca.explained_variance_ratio_)
+#Straight linear regression
+a = pd.get_dummies(cleaned['ZIP'],drop_first = False, sparse=True)
+acols = a.columns
+cleaned = cleaned.join(a)
+t = ['BEDS','BATHS','SQFT', 'BUILDING_TYPE_APT','BUILDING_TYPE_COMM', 'BUILDING_TYPE_CON','BUILDING_TYPE_SFR', 'GARAGE_Y', 'POOL_Y','TIME']+acols
+X = cleaned[t].fillna(0)
+X_train, X_test, y_train, y_test = train_test_split(X, cleaned['logrent'], test_size=0.2)
+y_test = np.array(y_test)
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+mse = 0
+for i in range(len(y_test)):
+    mse+= (y_test[i]-y_pred[i])**2
+mse = mse/len(y_test)
+
+#PCA
+del sample, samps, one, new_samps
+gc.collect()
+a = pd.get_dummies(cleaned['ZIP'],drop_first = False, sparse=True)
+ab = a.sparse.to_coo().tocsr()
+pca = sc.decomposition.TruncatedSVD(200)
+d = pca.fit(ab)
+d = pd.DataFrame(d)
+pca_set = cleaned.join(d)
+colpca = ['BEDS','BATHS','SQFT', 'BUILDING_TYPE_APT','BUILDING_TYPE_COMM', 'BUILDING_TYPE_CON','BUILDING_TYPE_SFR', 'GARAGE_Y', 'POOL_Y','TIME']+d.columns
+Xpca = pca_set[colpca].fillna(0)
+X_trainpca, X_testpca, y_trainpca, y_testpca = train_test_split(Xpca, pca_set['logrent'], test_size=0.2)
+y_testpca = np.array(y_testpca)
+model = LinearRegression()
+model.fit(X_trainpca, y_trainpca)
+y_predpca = model.predict(X_testpca)
+msepca = 0
+for i in range(len(y_testpca)):
+    msepca += (y_testpca[i]-y_predpca[i])**2
+msepca = msepca/len(y_testpca)
+
+#Neural net 
+x = sc.preprocessing.StandardScaler().fit_transform(X)
+y = (np.array(pca_set['logrent']) - np.mean(np.array(pca_set['logrent'])))/np.std(np.array(pca_set['logrent']))
+nX_train, nX_test, ny_train, ny_test = sc.model_selection.train_test_split(x, y, test_size=0.2)
+hidden_units = 20
+activation = 'sigmoid'
+learning_rate = 0.05
+epochs = 25
+    # [5,10,25,50,75,100]
+batch_size = 32
+neural_model = keras.models.Sequential()
+neural_model.add(keras.layers.Dense(input_dim=len(X.columns),
+                                 units=hidden_units,
+                                 activation=activation))
+    # add the output layer
+neural_model.add(keras.layers.Dense(input_dim=hidden_units,
+                                 units=1))
+# define our loss function and optimizer
+neural_model.compile(loss='MeanSquaredError',
+                  # Adam is a kind of gradient descent
+    optimizer=keras.optimizers.Adam(learning_rate=.01),
+    metrics=['mse'])
+execute = neural_model.fit(nX_train, ny_train, epochs=epochs, batch_size=batch_size)
+test_acc = neural_model.evaluate(nX_test, ny_test)
 
 
 
