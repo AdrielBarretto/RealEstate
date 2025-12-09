99a02d1953ecd3130ddad00924b6b8db10107e0b
Adriel Barretto <addybarretto@gmail.com>
2025-12-09 17:01:06 -0500

    Nearly final changes

---
+++ cleaningcode.py
@@ -0,0 +1,58 @@
+import os
+import pandas as pd 
+import polars as pl
+import numpy as np 
+import sklearn as sc
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import SGDRegressor
+from sklearn.preprocessing import StandardScaler, MaxAbsScaler
+from sklearn.pipeline import make_pipeline
+from scipy.stats import invwishart
+from scipy.stats import invgamma
+import math as math 
+from scipy.sparse import hstack
+from scipy.sparse import csr_matrix
+from keras.utils import Sequence
+from concurrent.futures import ThreadPoolExecutor
+# import tensorflow as tf
+# import keras
+from sklearn.metrics import r2_score
+import json
+import gc
+import matplotlib.pyplot as plt
+
+
+
+#CLEANING
+sample = pd.read_csv("a.csv")
+samps = sample.drop(['GRANITE', 'STAINLESS','GYM','DOORMAN','FURNISHED','LAUNDRY', 'CLUBHOUSE','LATITUDE','LONGITUDE','DESCRIPTION', 'GARAGE_COUNT','ADDRESS', 'COMPANY','ID','NEIGHBORHOOD','SCRAPED_TIMESTAMP','YEAR_BUILT','AVAILABLE_AT','AVAILABILITY_STATUS','ID'], axis=1)
+samps['YEAR'] =pd.to_datetime(samps['DATE_POSTED'])
+samps['MONTH'] = pd.to_numeric(samps['YEAR'].dt.month)
+samps['YEAR'] = pd.to_numeric(samps['YEAR'].dt.year)-2014
+
+#samps['MONTH'] = pd.to_numeric(pd.to_datetime(samps['DATE_POSTED'],dayfirst=True, format = "%m"))
+samps['TIME'] = 12*samps['YEAR']-1+samps['MONTH']
+one = pd.get_dummies(samps,columns = ['BUILDING_TYPE'], drop_first=False)
+new_samps = pd.get_dummies(one,columns = ['GARAGE','POOL'], drop_first=True)
+zen = len(samps['ZIP'].unique())
+new_samps.drop(columns = [ 'BUILDING_TYPE_TIME', 'BUILDING_TYPE_MH', 'BUILDING_TYPE_TH','DATE_POSTED','YEAR','MONTH'],axis =1,inplace=True)
+new_samps["logrent"] = np.log(new_samps["RENT_PRICE"])
+zip_counts = new_samps.groupby("ZIP")["ZIP"].transform("count")
+# cleaned = samps[zip_counts > 30]
+cleaned = new_samps[zip_counts > 50]
+cleaned = cleaned.dropna()
+del samps
+del new_samps
+gc.collect()
+cols = [
+    "BUILDING_TYPE_APT", "BUILDING_TYPE_COMM", "BUILDING_TYPE_CON",
+    "BUILDING_TYPE_SFR", "GARAGE_Y", "POOL_Y"
+]
+cleaned[cols] = cleaned[cols].replace(
+    {"True": 1, "False": 0, "true": 1, "false": 0}
+).astype(int)
+
+cleaned.to_csv("acleaned.csv")
+print("Data cleaning is done")
